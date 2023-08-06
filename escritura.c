fccd56d93d8dec69b262ed8696815321f5ab4c89
Dvargasar6 <120421265+Dvargasar6@users.noreply.github.com>
2023-08-05 21:57:28 -0500

    Add files via upload

---
+++ escritura.c
@@ -0,0 +1,9 @@
+#include<stdio.h>    //Standard input-output.header Librería estandar de entrada y salida. 
+int main()    //Función principal.
+{
+    printf("Escribiendo en C\t\n");   //Función de escritura.
+    printf("Imprimir una variable: %d\n",FILENAME_MAX); //%d es el formato de la variable.
+
+
+    return 0;
+}
