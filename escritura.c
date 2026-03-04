ceb1cdd4f86de6e46fbd034ce558a0d1d622780c
Daniel Felipe Vargas Arias <dvargasar@unal.edu.co>
2026-03-04 18:21:00 -0500

    Delete escritura.c

--- escritura.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>    //Standard input-output.header Librería estandar de entrada y salida. 
-int main()    //Función principal.
-{
-    printf("Escribiendo en C\t\n");   //Función de escritura.
-    printf("Imprimir una variable: %d\n",FILENAME_MAX); //%d es el formato de la variable.
-
-
-    return 0;
-}
